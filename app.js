438e33f696f17cfccfb03b096732dd758ed07b35
De Santi <matisant@f8ffc226934e.ant.amazon.com>
2021-07-02 12:04:43 -0300

    Adds api gateway response

--- app.js
+++ app.js
@@ -3,5 +3,12 @@ var aws = require('aws-sdk');
 exports.handler = function(event, context) {
   console.log('Hello 2!')
   console.log("REQUEST RECEIVED:\n" + JSON.stringify(event))
-  return
+  var res ={
+    statusCode: 200,
+    headers: {
+        "Content-Type": "*/*"
+    },
+    body: JSON.stringify(event)
+  };
+  return res
 }
